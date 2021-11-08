fc4f91e89cd80c6a80ddb33903c57c989f90311a
emerson_dalwampo <emerson.dalwampo@fireclay.ph>
2021-11-08 18:01:51 +0800

    Make Php server

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = config = {
-    mysqli: {
-        user: 'root',
-        host: 'localhost',
-        password: 'password',
-        database: 'sunhillian_etutor_master',
-    },
-    port: '4001',
-}
